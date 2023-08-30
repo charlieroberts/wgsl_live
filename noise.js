16d295176ff4b77d7f07929d04ca2aa136ea8a2d
charlieroberts <charlie@charlie-roberts.com>
2023-08-30 17:43:22 -0400

    perlin3 3d noise function

--- noise.js
+++ noise.js
@@ -3,7 +3,7 @@ fn rand2(n: vec2f) -> f32 {
   return fract(sin(dot(n, vec2f(12.9898, 4.1414))) * 43758.5453);
 }
 
-fn noise(n: vec2f) -> f32 {
+fn noise2(n: vec2f) -> f32 {
   let d = vec2f(0., 1.);
   let b = floor(n);
   let f = smoothstep(vec2f(0.), vec2f(1.), fract(n));
@@ -14,7 +14,7 @@ fn noise(n: vec2f) -> f32 {
 fn permute4(x: vec4f) -> vec4f { return ((x * 34. + 1.) * x) % vec4f(289.); }
 fn fade2(t: vec2f) -> vec2f { return t * t * t * (t * (t * 6. - 15.) + 10.); }
 
-fn perlin(P: vec2f) -> f32 {
+fn perlin2(P: vec2f) -> f32 {
   var Pi: vec4f = floor(P.xyxy) + vec4f(0., 0., 1., 1.);
   let Pf = fract(P.xyxy) - vec4f(0., 0., 1., 1.);
   Pi = Pi % vec4f(289.); // To avoid truncation effects in permutation
@@ -62,7 +62,7 @@ fn permute3(x: vec3f) -> vec3f {
 }
 
 //  MIT License. © Ian McEwan, Stefan Gustavson, Munrocket
-fn simplex(v: vec2f) -> f32 {
+fn simplex2(v: vec2f) -> f32 {
     let C = vec4(
         0.211324865405187, // (3.0-sqrt(3.0))/6.0
         0.366025403784439, // 0.5*(sqrt(3.0)-1.0)
@@ -117,24 +117,81 @@ fn hash23(p: vec2f) -> vec3f {
   return fract(sin(q) * 43758.5453);
 }
 
-fn voronoi(x: vec2f, u: f32, v: f32) -> f32 {
-  let p = floor(x);
-  let f = fract(x);
-  let k = 1. + 63. * pow(1. - v, 4.);
-  var va: f32 = 0.;
-  var wt: f32 = 0.;
-  for(var j: i32 = -2; j <= 2; j = j + 1) {
-    for(var i: i32 = -2; i <= 2; i = i + 1) {
-      let g = vec2f(f32(i), f32(j));
-      let o = hash23(p + g) * vec3f(u, u, 1.);
-      let r = g - f + o.xy;
-      let d = dot(r, r);
-      let ww = pow(1. - smoothstep(0., 1.414, sqrt(d)), k);
-      va = va + o.z * ww;
-      wt = wt + ww;
-    }
-  }
-  return va / wt;
-}`
+// MIT License. © Stefan Gustavson, Munrocket
+//
+fn taylorInvSqrt4(r: vec4f) -> vec4f { return 1.79284291400159 - 0.85373472095314 * r; }
+fn fade3(t: vec3f) -> vec3f { return t * t * t * (t * (t * 6. - 15.) + 10.); }
+
+fn perlin3(P: vec3f) -> f32 {
+  var Pi0 : vec3f = floor(P); // Integer part for indexing
+  var Pi1 : vec3f = Pi0 + vec3f(1.); // Integer part + 1
+  Pi0 = Pi0 % vec3f(289.);
+  Pi1 = Pi1 % vec3f(289.);
+  let Pf0 = fract(P); // Fractional part for interpolation
+  let Pf1 = Pf0 - vec3f(1.); // Fractional part - 1.
+  let ix = vec4f(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  let iy = vec4f(Pi0.yy, Pi1.yy);
+  let iz0 = Pi0.zzzz;
+  let iz1 = Pi1.zzzz;
+
+  let ixy = permute4(permute4(ix) + iy);
+  let ixy0 = permute4(ixy + iz0);
+  let ixy1 = permute4(ixy + iz1);
+
+  var gx0: vec4f = ixy0 / 7.;
+  var gy0: vec4f = fract(floor(gx0) / 7.) - 0.5;
+  gx0 = fract(gx0);
+  var gz0: vec4f = vec4f(0.5) - abs(gx0) - abs(gy0);
+  var sz0: vec4f = step(gz0, vec4f(0.));
+  gx0 = gx0 + sz0 * (step(vec4f(0.), gx0) - 0.5);
+  gy0 = gy0 + sz0 * (step(vec4f(0.), gy0) - 0.5);
+
+  var gx1: vec4f = ixy1 / 7.;
+  var gy1: vec4f = fract(floor(gx1) / 7.) - 0.5;
+  gx1 = fract(gx1);
+  var gz1: vec4f = vec4f(0.5) - abs(gx1) - abs(gy1);
+  var sz1: vec4f = step(gz1, vec4f(0.));
+  gx1 = gx1 - sz1 * (step(vec4f(0.), gx1) - 0.5);
+  gy1 = gy1 - sz1 * (step(vec4f(0.), gy1) - 0.5);
+
+  var g000: vec3f = vec3f(gx0.x, gy0.x, gz0.x);
+  var g100: vec3f = vec3f(gx0.y, gy0.y, gz0.y);
+  var g010: vec3f = vec3f(gx0.z, gy0.z, gz0.z);
+  var g110: vec3f = vec3f(gx0.w, gy0.w, gz0.w);
+  var g001: vec3f = vec3f(gx1.x, gy1.x, gz1.x);
+  var g101: vec3f = vec3f(gx1.y, gy1.y, gz1.y);
+  var g011: vec3f = vec3f(gx1.z, gy1.z, gz1.z);
+  var g111: vec3f = vec3f(gx1.w, gy1.w, gz1.w);
+
+  let norm0 = taylorInvSqrt4(
+      vec4f(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 = g000 * norm0.x;
+  g010 = g010 * norm0.y;
+  g100 = g100 * norm0.z;
+  g110 = g110 * norm0.w;
+  let norm1 = taylorInvSqrt4(
+      vec4f(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 = g001 * norm1.x;
+  g011 = g011 * norm1.y;
+  g101 = g101 * norm1.z;
+  g111 = g111 * norm1.w;
+
+  let n000 = dot(g000, Pf0);
+  let n100 = dot(g100, vec3f(Pf1.x, Pf0.yz));
+  let n010 = dot(g010, vec3f(Pf0.x, Pf1.y, Pf0.z));
+  let n110 = dot(g110, vec3f(Pf1.xy, Pf0.z));
+  let n001 = dot(g001, vec3f(Pf0.xy, Pf1.z));
+  let n101 = dot(g101, vec3f(Pf1.x, Pf0.y, Pf1.z));
+  let n011 = dot(g011, vec3f(Pf0.x, Pf1.yz));
+  let n111 = dot(g111, Pf1);
+
+  var fade_xyz: vec3f = fade3(Pf0);
+  let temp = vec4f(f32(fade_xyz.z)); // simplify after chrome bug fix
+  let n_z = mix(vec4f(n000, n100, n010, n110), vec4f(n001, n101, n011, n111), temp);
+  let n_yz = mix(n_z.xy, n_z.zw, vec2f(f32(fade_xyz.y))); // simplify after chrome bug fix
+  let n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+  return 2.2 * n_xyz;
+}
+`
 
 export default n
